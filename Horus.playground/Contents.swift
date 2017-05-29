ced1551b9a43ee246bd61183658cb0bf6c8ae932
Caynan Sousa <caynanvls@gmail.com>
2017-05-29 09:03:29 -0300

    providing better examples in playgroung

--- Horus.playground/Contents.swift
+++ Horus.playground/Contents.swift
@@ -7,8 +7,11 @@ let i: Int16 = try bin.scanValue(start: 0, length: 2)
 let i2: Int16 = try bin.get(at: 0)
 
 // Working with Float point numbers.
-let floatBin: Binary = [0x71, 0x3d, 0x0a, 0xd7, 0xa3, 0x10, 0x45, 0x40]
-let f: Double = try floatBin.get(at: 0)
+let doubleBin: Binary = [0xc9, 0xe5, 0x3f, 0xa4, 0xdf, 0xbe, 0x05, 0x40]
+let e: Double = try doubleBin.get(at: 0)
+
+let floatBin: Binary = [0xd0, 0x0f, 0x49, 0x40]
+let pi: Float = try floatBin.scanValue(start: 0, length: 4)
 
 // Rolling with Strings.
 let binStr: Binary = [0x48, 0x6F, 0x72, 0x75, 0x73, 0x00] // => "Horus\0"
